451d562b0295e55be4ab6107927000ec90ef709d
Sakai <sd99gqgo7@gmail.com>
2018-06-15 19:11:53 +0900

    Add to project files.

--- Assets/Scripts/InPipeline.cs
+++ Assets/Scripts/InPipeline.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class InPileline : MonoBehaviour {
 
+    public Vector3 inWaterPosition;
+    public bool isInWater { get; set; }
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        isInWater = false;
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
--- Assets/Scripts/OutPipeline.cs
+++ Assets/Scripts/OutPipeline.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class OutPipeline : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public InPileline inPipe;
+    public Vector3 outWaterPosition;
+
+    // Use this for initialization
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
