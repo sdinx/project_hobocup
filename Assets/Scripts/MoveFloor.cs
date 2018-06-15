96405ed29068d0cc04b634a7ab89ce030866a177
Sakai <sd99gqgo7@gmail.com>
2018-06-15 20:53:51 +0900

    Update map objects.

---
+++ Assets/Scripts/MoveFloor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFloor : MonoBehaviour {
+
+    public Vector3 movePower;
+    public Vector3 moveLimit;
+
+    private Vector3 currentPos;
+    private bool moveDirection;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentPos = new Vector3();
+        currentPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+    }
+}
